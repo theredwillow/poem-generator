cb4a4447882cfbf23ac3178250f428441a32830c
theredwillow <theredwillows@gmail.com>
2020-03-14 16:07:48 -0500

    Added hover rhyme scheme feature

--- src/Poem/Scheme/PoemGenerator/index.jsx
+++ src/Poem/Scheme/PoemGenerator/index.jsx
@@ -1,39 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 import {createPoem} from "./examplePoem";
 import "./index.css";
 
 export const PoemGenerator = ({scheme}) => {
-    if (scheme==="") {
-      return <i>(Provide a rhyme scheme to see a poem here.)</i>;
-    }
-    scheme = scheme.trim().toUpperCase();
-    
-    const poem = createPoem(scheme);
-    const stanzas = poem.reduce((acc, line, i) => {
-      const newStanza = (i===0 || line.stanza !== poem[i-1].stanza);
-      if (newStanza) { return [...acc, [line]]; }
-      const thisStanza = acc.pop();
-      return [...acc, [...thisStanza, line]];
-    }, []);
-  
-    const stanzaDisplays = stanzas.map((s, i) => {
-      // TODO Add letter-number classNames 
-      const lineDisplays = s.map((l,j) => (
-        <div key={`example-line-${j}`} className="example-line">
-          {l.letter}. {l.line}
-        </div>
-      ));
-      return (
-        <div key={i} className="example-stanza">
-          <div className="title">Stanza {i}</div>
-          {lineDisplays}
-        </div>
-      );
-    });
+
+  const [hover, setHover] = useState();
+
+  if (scheme==="") {
+    return <i>(Provide a rhyme scheme to see a poem here.)</i>;
+  }
+  scheme = scheme.trim().toUpperCase();
+
+  const poem = createPoem(scheme);
+  const stanzas = poem.reduce((acc, line, i) => {
+    const newStanza = (i===0 || line.stanza !== poem[i-1].stanza);
+    if (newStanza) { return [...acc, [line]]; }
+    const thisStanza = acc.pop();
+    return [...acc, [...thisStanza, line]];
+  }, []);
+
+  const stanzaDisplays = stanzas.map((s, i) => {
+    // TODO Add letter-number classNames 
+    const lineDisplays = s.map((l,j) => (
+      <div
+        key={`example-line-${j}`}
+        className={`example-line letter-${l.letter}`}
+        onMouseOver={() => setHover(l.letter)}
+        onMouseOut={() => setHover()}
+      >
+        {l.line}
+      </div>
+    ));
     return (
-      <div id="example-poem">
-        <b>"Poem with the <i>{scheme}</i> rhyme scheme"</b><br/>
-        {stanzaDisplays}
+      <div key={i} className="example-stanza">
+        <div className="title">Stanza {i}</div>
+        {lineDisplays}
       </div>
     );
+  });
+  const responsiveScheme = scheme.split('').map((letter) => {
+    if (letter === " ") { return ' '; }
+    return (
+      <span
+        key={letter}
+        className={`scheme-letter letter-${letter}`}
+        onMouseOver={() => setHover(letter)}
+        onMouseOut={() => setHover()}
+      >
+        {letter}
+      </span>
+    );
+  });
+  return (
+    <div id="example-poem" className={(hover) ? `hover-${hover}` : ''}>
+      <b>"Poem with the {responsiveScheme} rhyme scheme"</b><br/>
+      {stanzaDisplays}
+    </div>
+  );
 };
