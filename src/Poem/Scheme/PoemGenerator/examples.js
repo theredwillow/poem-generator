737c76dbe13ca37ae18c313818a4553ebb8e9116
theredwillow <theredwillows@gmail.com>
2020-03-14 23:35:33 -0500

    Added more example lines

--- src/Poem/Scheme/PoemGenerator/examples.js
+++ src/Poem/Scheme/PoemGenerator/examples.js
@@ -12,7 +12,7 @@ export const exampleLines = [
   ],
   [
     `You chose a scheme.`,
-    `It isn't too extreme.`,
+    `It's not too extreme.`,
     `I like ice cream.`,
     `Is there a theme?`,
     `Airflow, or air stream?`,
@@ -31,5 +31,49 @@ export const exampleLines = [
     `...my misplaced affection.`,
     `Death by Vivisection.`,
     `...imperfection.`
+  ],
+  [
+    `Walking through the fog`,
+    `Reciting my monologue`,
+    `I'm the top dog`,
+    `Drink some eggnog`,
+    `See through the smog`,
+    `Sent to the gulag.`,
+    `Obedient, little lap dog`
+  ],
+  [
+    `I said, 'Uhmm? ...yes!'`,
+    `Too much to process...`,
+    `Faux pax to address`,
+    `Done through due process`,
+    `I need to confess`,
+    `I want to progress`,
+    `This was a success`,
+    `My project to obssess`,
+    `See mistakes? Reassess!`
+  ],
+  [
+    `The sky is gray.`,
+    `Read my resume.`,
+    `I need to say...`,
+    `Keep them at bay!`,
+    `Let's go and play!`,
+    `Write an essay.`,
+    `You will soon pay!`,
+    `I need to convey:`,
+    `See you Friday!`,
+    `Gone on holiday`,
+    `Caught in the fray`,
+    `To my dismay`,
+    `My pain, on display!`,
+    `My opinion did sway`,
+    `You have to stay!`,
+    `See you in May!`,
+    `I struggle all day`,
+    `Today's my birthday.`,
+    `No need to betray`,
+    `working at the cafe`,
+    `Eat the whole buffet!`,
+    `Read the Steele Dossier!`
   ]
 ];
