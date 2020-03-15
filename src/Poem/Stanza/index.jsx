9904aac154bad8c1cd1a3ba79064b49a21b82472
theredwillow <theredwillows@gmail.com>
2020-03-14 23:56:26 -0500

    Toggle lines/notes in stanzas

--- src/Poem/Stanza/index.jsx
+++ src/Poem/Stanza/index.jsx
@@ -1,16 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import Line from "./Line";
 
 const Stanza = ({ stanza, i }) => {
+
+  const [display, setDisplay] = useState('lines');
+
   const lines = stanza.split("");
   const lineDisplays = lines.map((letter, i) => <Line key={`line-${i}`} letter={letter} />);
   return (
-    <div className="stanza">
+    <div className={`stanza display-${display}`}>
       Stanza {i + 1}<br/>
-      <div className="line-container">
+      <button onClick={() => setDisplay("lines")} disabled={(display === "lines")}>Lines</button>
+      <button onClick={() => setDisplay("notes")} disabled={(display === "notes")}>Notes</button><br/>
+      <div className="lines">
         {lineDisplays}
       </div>
-      <textarea className="stanza-notes" rows={lines.length} />
+      <textarea className="notes" rows={lines.length * 2} />
     </div>
   );
 };
