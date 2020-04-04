8378dbec501bbc644786178b656ce588db76f788
theredwillow <theredwillows@gmail.com>
2020-04-04 16:56:47 -0500

    Cleaned up the stanza display mode tabs

--- src/Poem/Stanza/index.jsx
+++ src/Poem/Stanza/index.jsx
@@ -4,19 +4,41 @@ import "./index.css";
 
 const Stanza = ({ stanza, i }) => {
 
-  const [display, setDisplay] = useState('lines');
+  const [mode, setMode] = useState('lines');
 
   const lines = stanza.split("");
   const lineDisplays = lines.map((letter, i) => <Line key={`line-${i}`} letter={letter} />);
   return (
-    <div className={`stanza display-${display}`}>
-      Stanza {i + 1}<br/>
-      <button onClick={() => setDisplay("lines")} disabled={(display === "lines")}>Lines</button>
-      <button onClick={() => setDisplay("notes")} disabled={(display === "notes")}>Notes</button><br/>
-      <div className="lines">
+    <div className="stanza" data-mode={mode}>
+      <div className="header">
+        <span className="number">Stanza {i + 1}</span>
+        
+        <div className="tabs">
+          <button
+            className="lines"
+            onClick={() => setMode("lines")}
+            disabled={(mode === "lines")}
+          >
+            Lines
+          </button>
+          
+          <button
+            className="notes"
+            onClick={() => setMode("notes")}
+            disabled={(mode === "notes")}
+          >
+            Notes
+          </button>
+        </div>
+      </div>
+      
+      <div className="display lines">
         {lineDisplays}
       </div>
-      <textarea className="notes" rows={lines.length * 2} />
+      
+      <div className="display notes">
+        <textarea rows={lines.length * 2} />
+      </div>
     </div>
   );
 };
