46f2cad12de4569756e6c0c8df0616442f0cd334
theredwillow <theredwillows@gmail.com>
2020-04-05 14:57:49 -0500

    W/new checker, renamed classNames to real letters

--- src/Poem/Stanza/Line/index.jsx
+++ src/Poem/Stanza/Line/index.jsx
@@ -1,17 +1,11 @@
-import React, { useContext } from "react";
-import { PoemContext } from "../../context";
+import React from "react";
 
-const Line = ({ letter }) => {
-  const {letters} = useContext(PoemContext);
-  const letterNumber = letters.indexOf(letter) + 1 || "x";
-
-  return (
-    <div className={`line letter-${letterNumber}`}>
-      {/* FIXME This needs to be handled by context */}
-      <input type="text" />
-      <div className="line-letter">{letter}</div>
-    </div>
-  );
-};
+const Line = ({ letter }) => (
+  <div className={`line letter-${letter}`}>
+    {/* FIXME Input should be handled by context */}
+    <input type="text" />
+    <div className="line-letter">{letter}</div>
+  </div>
+);
 
 export default Line;
