8231bded288effa4df2b005764e755b90a3191e2
theredwillow <theredwillows@gmail.com>
2020-11-17 21:04:47 -0600

    Updates

---
+++ precache-manifest.96ec47af6dc1395f73ed9367b55457c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f05df15691a49c2fa878c2867e94f4",
+    "url": "/poem-generator/index.html"
+  },
+  {
+    "revision": "4a8d5ffecd9c23c5cbfe",
+    "url": "/poem-generator/static/css/main.a42ae581.chunk.css"
+  },
+  {
+    "revision": "5f8ac6d75b2c8ecc2cd7",
+    "url": "/poem-generator/static/js/2.3e411f74.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/poem-generator/static/js/2.3e411f74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8d5ffecd9c23c5cbfe",
+    "url": "/poem-generator/static/js/main.8e1a643c.chunk.js"
+  },
+  {
+    "revision": "9f1feaae66778219a212",
+    "url": "/poem-generator/static/js/runtime-main.348cb1f9.js"
+  }
+]);
